6e43b99915cded6b6db1a387e7582e64cadf0322
Luis Angel <angelfgdeveloper@gmail.com>
2024-12-13 03:42:39 -0600

    Test basicos de saldo

--- src/test/java/org/angelfg/ejemplos/models/CuentaTest.java
+++ src/test/java/org/angelfg/ejemplos/models/CuentaTest.java
@@ -21,4 +21,12 @@ class CuentaTest {
         assertTrue(actual.equals("Luis"));
     }
 
+    @Test
+    void test_saldo_cuenta() {
+        Cuenta cuenta = new Cuenta("Luis", BigDecimal.valueOf(1000.12345));
+        assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0); // menor que cero
+        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0); // mayor que cero
+    }
+
 }
