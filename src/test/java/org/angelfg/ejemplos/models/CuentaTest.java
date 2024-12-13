113a9f5fa1be867d03b52d4efbeed4c2dd8b9995
Luis Angel <angelfgdeveloper@gmail.com>
2024-12-13 04:00:52 -0600

    Uso de TDD y test de debito y credito

--- src/test/java/org/angelfg/ejemplos/models/CuentaTest.java
+++ src/test/java/org/angelfg/ejemplos/models/CuentaTest.java
@@ -17,6 +17,7 @@ class CuentaTest {
         String esperando = "Luis";
         String actual = cuenta.getPersona();
 
+        assertNotNull(actual);
         assertEquals(esperando, actual);
         assertTrue(actual.equals("Luis"));
     }
@@ -24,6 +25,7 @@ class CuentaTest {
     @Test
     void test_saldo_cuenta() {
         Cuenta cuenta = new Cuenta("Luis", BigDecimal.valueOf(1000.12345));
+        assertNotNull(cuenta.getSaldo());
         assertEquals(1000.12345, cuenta.getSaldo().doubleValue()); // compara por referencia
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0); // menor que cero
         assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0); // mayor que cero
@@ -36,6 +38,26 @@ class CuentaTest {
 
         // assertNotEquals(cuenta1, cuenta2); // No son referencias iguales (objetos diferentes en memoria) -> true
         assertEquals(cuenta1, cuenta2); // Falla, ahora pasa porque se coloco un equals override
+    }
+
+    @Test
+    void test_debito_cuenta() {
+        Cuenta cuenta = new Cuenta("Luis", BigDecimal.valueOf(1000.12345));
+        cuenta.debito(BigDecimal.valueOf(100));
+
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900.12345, cuenta.getSaldo().doubleValue());
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+    }
+
+    @Test
+    void test_credito_cuenta() {
+        Cuenta cuenta = new Cuenta("Luis", BigDecimal.valueOf(1000.12345));
+        cuenta.credito(BigDecimal.valueOf(100));
+
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(1100.12345, cuenta.getSaldo().doubleValue());
+        assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
     }
 
 }
